7862fef0bf78e4fcc39bff0a9bc98fb6acd3de6f
laif <laif@gmail.com>
2020-08-17 11:26:53 +0200

    Another test

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+#.OBJECTS kann ich machen, wenns in der selben directory ist, somit nichts weiter als Pfad angegeben werden muss!
+
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+#mit all werden ALLE auf der seite gezeigt
